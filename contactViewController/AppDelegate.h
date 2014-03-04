48b87b7cc0b308a1a3ec02acfa550cdb78a3f880
catinclay <catinclay@gmail.com>
2014-03-03 23:08:07 -0500

    Initial Commit

---
+++ contactViewController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  contactViewController
+//
+//  Created by catinclay on 3/3/14.
+//  Copyright (c) 2014 catinclay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ contactViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  contactViewController
+//
+//  Created by catinclay on 3/3/14.
+//  Copyright (c) 2014 catinclay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
